85c1c09d1e067899fb28a35c5417a790b515ed21
antonio-estevam <jhonnyimmbe@gmail.com>
2025-12-22 11:40:51 +0000

    :feat Configure the Postgres and Hibernate environment to map your entities, manipulate repository-type interfaces, inject dependencies, and manage environment variables.

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -3,34 +3,41 @@ package br.com.alura.screenmatch.principal;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.model.Episodio;
+import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.repository.SerieRepository;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
-import java.util.ArrayList;
-import java.util.DoubleSummaryStatistics;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Principal {
 
+    private final SerieRepository repository;
     private Scanner leitura = new Scanner(System.in);
     private ConsumoApi consumo = new ConsumoApi();
     private ConverteDados conversor = new ConverteDados();
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=6585022c";
 
+    private List<DadosSerie> dadosSeries = new ArrayList<>();
+
+    public Principal(SerieRepository repository) {
+        this.repository = repository;
+    }
+
     public void exibeMenu() {
+        var  opcao = -1;
+        while (opcao != 0){
         var menu = """
                 1 - Buscar séries
                 2 - Buscar episódios
-                
+                3 - Listar séries buscadas
                 0 - Sair                                 
                 """;
 
         System.out.println(menu);
-        var opcao = leitura.nextInt();
+        opcao = leitura.nextInt();
         leitura.nextLine();
 
         switch (opcao) {
@@ -40,16 +47,24 @@ public class Principal {
             case 2:
                 buscarEpisodioPorSerie();
                 break;
+            case 3:
+                listarSeriesBuscadas();
+                break;
             case 0:
                 System.out.println("Saindo...");
                 break;
             default:
                 System.out.println("Opção inválida");
         }
+        }
     }
 
     private void buscarSerieWeb() {
         DadosSerie dados = getDadosSerie();
+        Serie serie = new Serie(dados);
+        //dadosSeries.add(dados);
+
+        repository.save(serie);
         System.out.println(dados);
     }
 
@@ -71,5 +86,13 @@ public class Principal {
             temporadas.add(dadosTemporada);
         }
         temporadas.forEach(System.out::println);
+    }
+    private void listarSeriesBuscadas(){
+        List<Serie> series =  repository.findAll();
+        series.stream()
+                .sorted(Comparator.comparing(Serie::getGenero))
+                .forEach(System.out::println);
+
+
     }
 }
